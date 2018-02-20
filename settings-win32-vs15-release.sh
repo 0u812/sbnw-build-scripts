60a3fcb5aa348d3fba0306f187ab277bd0e9b789
0u812 <0u812@users.noreply.github.com>
2018-02-20 14:02:03 -0800

    add release to path

--- settings-win32-vs15-release.sh
+++ settings-win32-vs15-release.sh
@@ -6,12 +6,12 @@ INSTALL_ROOT="C:/Users/libsbml/Documents/devel/install"
 
 CMAKE="C:/Users/libsbml/Downloads/cmake-3.11.0-rc1-win64-x64/bin/cmake"
 
-VCRUNTIME1="C:/Users/libsbml/Downloads/libsbml/libSBML_dependencies_vs14_release_x64/libSBML-Dependencies-1.0.0-b1-win64/bin/msvcp140.dll"
-VCRUNTIME2="C:/Users/libsbml/Downloads/libsbml/libSBML_dependencies_vs14_release_x64/libSBML-Dependencies-1.0.0-b1-win64/bin/vcruntime140.dll"
+# VCRUNTIME1="C:/Users/libsbml/Downloads/libsbml/libSBML_dependencies_vs14_release_x64/libSBML-Dependencies-1.0.0-b1-win64/bin/msvcp140.dll"
+# VCRUNTIME2="C:/Users/libsbml/Downloads/libsbml/libSBML_dependencies_vs14_release_x64/libSBML-Dependencies-1.0.0-b1-win64/bin/vcruntime140.dll"
 
 # libSBML deps
 LIBSBML_NAME="libsbml-5.16.0-experimental"
-LIBSBML_DEP_DIR="C:/Users/libsbml/Downloads/libsbml-dep-win32-vs15-static"
+LIBSBML_DEP_DIR="C:/Users/libsbml/Downloads/libsbml-dep-win32-vs15-static-release"
 LIBBZIP2="$LIBSBML_DEP_DIR/lib/libbz2.lib"
 LIBBZIP2_INCLUDE="$LIBSBML_DEP_DIR/include"
 LIBICONV="$LIBSBML_DEP_DIR/lib/libiconv.lib"
