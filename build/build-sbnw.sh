3d1a3ef94c7b167798698184655292fe2796503b
0u812 <0u812@users.noreply.github.com>
2018-02-20 14:25:38 -0800

    add sbnw scripts

---
+++ build/build-sbnw.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+THIS_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+
+source $THIS_DIR/../global/global-$PLATFORM.sh
+source $THIS_DIR/../sbnw/build.sh
---
+++ sbnw/build.sh
@@ -0,0 +1,20 @@
+#/usr/bin/env bash
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+THIS_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+
+SRC_ROOT=$SRC_ROOT/sbnw
+INSTALL_DIR="$SBNW_INSTALL_DIR"
+BUILD_DIR="$BUILD_ROOT/$SBNW_INSTNAME"
+
+BUILD_TYPE=Release
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+"$CMAKE" "$CMAKE_GEN" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DLIBSBML_DIR=$LIBSBML_INSTALL_DIR $SRC_ROOT
+eval $CMAKE_BUILD_CMD
